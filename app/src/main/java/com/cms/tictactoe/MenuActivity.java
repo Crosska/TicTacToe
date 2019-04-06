f68b65cac41c4c01aa7faa4440ff0e0bf815c8b0
Vladislav Muzichin <musical.vg@gmail.com>
2019-04-06 11:05:27 +0300

    Main menu activity view was developed. Exit button works.

--- app/src/main/java/com/cms/tictactoe/MenuActivity.java
+++ app/src/main/java/com/cms/tictactoe/MenuActivity.java
@@ -2,6 +2,7 @@ package com.cms.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void start_button_pressed(View view) {
+    }
+
+    public void settings_button_pressed(View view) {
+    }
+
+    public void exit_button_pressed(View view) {
+        finish();
     }
 }
